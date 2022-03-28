29ac04fcace24a61c6ab496b3b337374e4abea91
allentasha <allentasha860@gmail.com>
2022-03-28 14:05:44 +0300

    The first java assignment

---
+++ functions.js
@@ -0,0 +1,23 @@
+function checkAge() {
+    let age = 15;3
+    if (age>=16) {
+        console.log("You may proceed and attend");
+} else{
+    console.log("sorry we cant let you in.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java assign.js
@@ -0,0 +1,10 @@
+const Temperature=45;
+if(Temperature>=20 && Temperature<=36){
+    console.log("normal")
+}
+else if(Temperature>=37 && Temperature<=40){
+    console.log("above normal")
+}
+else{
+    console.log("erratic")
+}
